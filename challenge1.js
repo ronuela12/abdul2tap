a87d77827905a52ec5765b360a48a8141608ca4d
ronuela12 <59582953+ronuela12@users.noreply.github.com>
2020-01-06 12:36:05 -1000

    Add files via upload

---
+++ challenge1.js
@@ -0,0 +1,50 @@
+
+function days(){ 
+    
+    let age = prompt("How old are you?");
+    //let ageInDays = (age * 365);
+    
+    //Made If else statement restricting the numbers used and only numbers
+    if ((age >= 0) && (age < 101)) {
+        let ageInDays = (age * 365);
+
+        let x = document.createElement('x');
+    //Create "answer" to create a string --> shown = createTextNode = (String + Integer + String);
+    let answer = document.createTextNode('You are ' + ageInDays + ' days old!');
+    //Program still continues without setAttribute
+    x.setAttribute('id', 'ageInDays');
+    //"x" is now "answer"
+    x.appendChild(answer);
+    //"x" will print under the id "result" in index.html
+    document.getElementById('result').appendChild(x);
+    function end(){
+        document.getElementById(ageInDays).remove();
+    }
+    }
+    else{
+       alert("Error: Enter numbers between 1 and 100");
+    }
+    //let ageInDays = (age * 365);
+
+
+    //Sends the results
+    //create "x" to get an element
+    //let x = document.createElement('x');
+    //Create "answer" to create a string --> shown = createTextNode = (String + Integer + String);
+    //let answer = document.createTextNode('You are ' + ageInDays + ' days old!');
+    //Program still continues without setAttribute
+    //x.setAttribute('id', 'ageInDays');
+    //"x" is now "answer"
+    //x.appendChild(answer);
+    //"x" will print under the id "result" in index.html
+    //document.getElementById('result').appendChild(x);
+    
+}
+//function end(){
+//    document.getElementById('ageInDays').remove();
+//}
+
+function myFunction() {
+    confirm("Are you above the age of 18?");
+    
+}
